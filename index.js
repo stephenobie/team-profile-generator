40698360b5ec9bf85cbd5e77625d6c068bbe65e4
Stephen Obie <sobie31@outlook.com>
2021-06-07 20:57:55 -0400

    added to html.js and soem html

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const Manager = require("./front-end/index");
-const Engineer = require("./front-end/engineer");
-const Intern = require("./front-end/intern");
-const inquirer = require("inquirer");
-const inquirer = require("inquirer");
-const inquirer = require("inquirer");
+const path = require("path");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const render = require("./lib/html");
+
+init = () => {
+    return inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "Who is the team Manager?",
+          name: "name",
+        },
+        {
+          type: "input",
+          message: "What's the manager's ID number?:",
+          name: "id",
+        },
+        {
+          type: "input",
+          message: "Manager's email address:",
+          name: "email",
+        },
+        {
+          type: "input",
+          message: "Manager's office number:",
+          name: "officeNumber",
+        },
+      ])
+      .then((managerResults) => {
+        managerResults.role = "Manager";
+        const { name, id, email, officeNumber, role } = managerResults;
+        const newManager = new Manager(name, id, email, officeNumber, role);
+        employees.push(newManager);
+        employeeType();
+      });
+  };
